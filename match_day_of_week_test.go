28c5489eff651c42095d8089b15058dc4f4fdd80
winebarrel <sugawara@winebarrel.jp>
2022-10-17 12:24:04 +0900

    Add match dow tests

--- match_day_of_week_test.go
+++ match_day_of_week_test.go
@@ -167,6 +167,22 @@ func TestMatchDayOfWeek(t *testing.T) {
 			},
 		},
 		{
+			exp: "* * ? * 2/0 *",
+			tests: []struct {
+				tm       time.Time
+				expected bool
+			}{
+				{time.Date(2022, 10, 2, 1, 1, 0, 0, time.UTC), false},
+				{time.Date(2022, 10, 3, 1, 1, 0, 0, time.UTC), true},
+				{time.Date(2022, 10, 4, 1, 1, 0, 0, time.UTC), false},
+				{time.Date(2022, 10, 5, 1, 1, 0, 0, time.UTC), false},
+				{time.Date(2022, 10, 6, 1, 1, 0, 0, time.UTC), false},
+				{time.Date(2022, 10, 7, 1, 1, 0, 0, time.UTC), false},
+				{time.Date(2022, 10, 8, 1, 1, 0, 0, time.UTC), false},
+				{time.Date(2022, 10, 9, 1, 1, 0, 0, time.UTC), false},
+			},
+		},
+		{
 			exp: "* * ? * 1/4 *",
 			tests: []struct {
 				tm       time.Time
