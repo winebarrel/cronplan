ad82c48ce9110f9ea448cc1c51611deceac995e1
winebarrel <sugawara@winebarrel.jp>
2022-10-19 18:07:10 +0900

    Add cronmatch command

--- cmd/cronmatch/flags.go
+++ cmd/cronmatch/flags.go
@@ -14,16 +14,16 @@ var (
 )
 
 type flags struct {
-	n    int
 	h    int
 	expr string
+	t    string
 }
 
 func init() {
 	cmdLine := flag.NewFlagSet(filepath.Base(os.Args[0]), flag.ExitOnError)
 
 	cmdLine.Usage = func() {
-		fmt.Fprintf(cmdLine.Output(), "Usage: %s [OPTION] CRON_EXPR\n", cmdLine.Name())
+		fmt.Fprintf(cmdLine.Output(), "Usage: %s [OPTION] CRON_EXPR DATE\n", cmdLine.Name())
 		cmdLine.PrintDefaults()
 	}
 
@@ -33,7 +33,6 @@ func init() {
 func parseFlags() *flags {
 	flags := &flags{}
 	flag.IntVar(&flags.h, "h", 0, "hour to add")
-	flag.IntVar(&flags.n, "n", 10, "number of next triggers")
 	showVersion := flag.Bool("version", false, "print version and exit")
 	flag.Parse()
 
@@ -45,15 +44,14 @@ func parseFlags() *flags {
 
 	if len(args) == 0 {
 		printUsageAndExit()
-	} else if len(args) > 1 {
+	} else if len(args) < 2 {
+		log.Fatal("too few arguments")
+	} else if len(args) > 2 {
 		log.Fatal("too many arguments")
 	}
 
 	flags.expr = strings.TrimSpace(args[0])
-
-	if flags.n < 1 {
-		log.Fatal("'-n' must be >= 1")
-	}
+	flags.t = strings.TrimSpace(args[1])
 
 	return flags
 }
