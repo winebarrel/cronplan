40e48fd858cd65e2d234078732a1ba0c716f3169
winebarrel <sugawara@winebarrel.jp>
2025-11-09 15:17:24 +0900

    Add cron example

--- _example/basic/go.mod
+++ _example/basic/go.mod
@@ -2,7 +2,7 @@ module main
 
 go 1.23.0
 
-replace github.com/winebarrel/cronplan/v2 => ../
+replace github.com/winebarrel/cronplan/v2 => ../../
 
 require github.com/winebarrel/cronplan/v2 v2.0.0-00010101000000-000000000000
 
---
+++ _example/cron/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/winebarrel/cronplan/v2"
+)
+
+func cron(expr string, from time.Time, proc func()) (<-chan struct{}, error) {
+	cron, err := cronplan.Parse(expr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	iter := cron.IterFrom(from)
+	waiter := make(chan struct{})
+
+	go func() {
+		for {
+			next := iter.Next()
+
+			if next.IsZero() {
+				break
+			}
+
+			<-time.After(time.Until(next))
+			go proc()
+		}
+
+		close(waiter)
+	}()
+
+	return waiter, nil
+}
+
+func main() {
+	startTime := time.Now().Add(1 * time.Minute)
+
+	waiter, err := cron("* * * * ? *", startTime, func() {
+		fmt.Println(time.Now())
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	<-waiter
+}
